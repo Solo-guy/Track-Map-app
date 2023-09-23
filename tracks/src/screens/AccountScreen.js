b376c6dd96ed2695a95718b5e21ad801fadea810
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-09-23 11:37:03 +0700

    day 7

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../Context/AuthContext';
 
@@ -9,7 +9,7 @@ const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
 
   return (
-    <SafeAreaView forceInset={{ top: 'always' }}>
+    <SafeAreaView>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signout} />
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { StyleSheet} from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { requestForegroundPermissionsAsync } from 'expo-location'; 
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestForegroundPermissionsAsync();
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView>
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     );
 };
@@ -16,3 +31,15 @@ const TrackCreateScreen = () => {
 const styles = StyleSheet.create({});
 
 export default TrackCreateScreen;
+
+
+// const startWatching = async () => {
+//     try {
+//       const { granted } = await requestPermissionsAsync();
+//       if (!granted) {
+//         throw new Error('Location permission not granted');
+//       }
+//     } catch (e) {
+//       setErr(e);
+//     }
+//   };
