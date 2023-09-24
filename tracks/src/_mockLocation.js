ee3e39bfe78205ed5d7211b4822482ec39ee307c
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-09-24 18:46:58 +0700

    day 8

---
+++ tracks/src/_mockLocation.js
@@ -0,0 +1,26 @@
+import * as Location from 'expo-location';
+
+const tenMettersWithDegrees = 0.0001;
+
+const getLocation = increment => {
+    return {
+        timestamp: 10000000,
+        coords: {
+            speed: 0,
+            heading: 0,
+            accuracy: 5,
+            altitudeAccuracy: 5,
+            longitude: -122.0312186 + increment * tenMettersWithDegrees,
+            latitude: 37.33233141 + increment * tenMettersWithDegrees
+        }
+    };
+};
+
+let counter = 0;
+setInterval(() => {
+    Location.EventEmitter.emit('Expo.locationChanged', {
+        watchId: Location._getCurrentWatchId(),
+        location: getLocation(counter)
+    });
+    counter++;
+}, 1000);
